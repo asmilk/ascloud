8e5f7e5b3849562a61097b81bda00f07f213f2b4
user <user@192.168.129.81>
2018-04-09 10:23:46 +0800

    12

--- src/main/java/asmilk/ascloud/config/LogConfigListener.java
+++ src/main/java/asmilk/ascloud/config/LogConfigListener.java
@@ -17,20 +17,6 @@ public class LogConfigListener implements ServletContextListener {
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
 		SLF4JBridgeHandler.install();
 		LOG.info("Installed root logger handler.");
-
-		// LOG.info("=====System Properties=====");
-		// Properties properties = System.getProperties();
-		// Set<Entry<Object, Object>> propSet = properties.entrySet();
-		// for (Entry<Object, Object> entry : propSet) {
-		// LOG.info("{}:{}", entry.getKey(), entry.getValue());
-		// }
-		//
-		// LOG.info("=====System Env=====");
-		// Map<String, String> map = System.getenv();
-		// Set<Entry<String, String>> mapSet = map.entrySet();
-		// for (Entry<String, String> entry : mapSet) {
-		// LOG.info("{}:{}", entry.getKey(), entry.getValue());
-		// }
 	}
 
 	public void contextDestroyed(ServletContextEvent sce) {
