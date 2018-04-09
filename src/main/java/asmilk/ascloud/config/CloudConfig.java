b03d5327ec8e56d8072da3fc3d2404052741eb8f
user <user@192.168.129.81>
2018-04-09 11:15:26 +0800

    13

--- src/main/java/asmilk/ascloud/config/CloudConfig.java
+++ src/main/java/asmilk/ascloud/config/CloudConfig.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 
 @ServiceScan
@@ -11,7 +13,9 @@ public class CloudConfig extends AbstractCloudConfig {
 
 	@Bean
 	public DataSource dataSource() {
-		return this.connectionFactory().dataSource();
+		PoolConfig poolConfig = new PoolConfig(5, 3000);
+		DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
+		return this.connectionFactory().dataSource(dataSourceConfig);
 	}
 
 }
