78fe225f9631e6596c223bcfc785a2ba3d7e64c4
user <user@192.168.129.81>
2018-04-25 16:18:09 +0800

    25

--- src/main/java/asmilk/ascloud/config/CloudConfig.java
+++ src/main/java/asmilk/ascloud/config/CloudConfig.java
@@ -1,22 +1,18 @@
 package asmilk.ascloud.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
-import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
-import org.springframework.context.annotation.Bean;
 
 @ServiceScan
 public class CloudConfig extends AbstractCloudConfig {
 
-	@Bean
-	public DataSource dataSource() {
-		PoolConfig poolConfig = new PoolConfig(5, 3000);
-		DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
-		return this.connectionFactory().dataSource("ElephantSQL-Tiny_Turtle", dataSourceConfig);
-	}
+	// @Bean
+	// public DataSource dataSource() {
+	// PoolConfig poolConfig = new PoolConfig(5, 3000);
+	// DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
+	// return this.connectionFactory().dataSource("ElephantSQL-Tiny_Turtle",
+	// dataSourceConfig);
+	// }
 
 	// @Bean
 	// public RedisConnectionFactory redisConnectionFactory() {
