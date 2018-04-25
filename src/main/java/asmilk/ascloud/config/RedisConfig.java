8b55299aa23606f83378f46daa670e068bf17ed3
user <user@192.168.129.81>
2018-04-25 15:46:45 +0800

    23

--- src/main/java/asmilk/ascloud/config/RedisConfig.java
+++ src/main/java/asmilk/ascloud/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package asmilk.ascloud.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,7 +8,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfig {
 
 	@Bean
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+	public RedisTemplate<String, String> redisTemplate(
+			@Qualifier("redisConnectionFactory") RedisConnectionFactory connectionFactory) {
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
 		redisTemplate.setConnectionFactory(connectionFactory);
 		return redisTemplate;
