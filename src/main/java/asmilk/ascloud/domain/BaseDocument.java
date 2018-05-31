81570433374321c8fcb664c568c08aaf76907ede
user <user@192.168.129.81>
2018-05-31 14:24:35 +0800

    52

---
+++ src/main/java/asmilk/ascloud/domain/BaseDocument.java
@@ -0,0 +1,59 @@
+package asmilk.ascloud.domain;
+
+import com.google.gson.annotations.SerializedName;
+
+public class BaseDocument {
+
+	@SerializedName("_id")
+	private String id;
+	@SerializedName("_rev")
+	private String revision;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRevision() {
+		return revision;
+	}
+
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((revision == null) ? 0 : revision.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseDocument other = (BaseDocument) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (revision == null) {
+			if (other.revision != null)
+				return false;
+		} else if (!revision.equals(other.revision))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/asmilk/ascloud/domain/Book.java
+++ src/main/java/asmilk/ascloud/domain/Book.java
@@ -2,9 +2,7 @@ package asmilk.ascloud.domain;
 
 import java.io.Serializable;
 
-import com.cloudant.client.api.model.Document;
-
-public class Book extends Document implements Serializable {
+public class Book extends BaseDocument implements Serializable {
 
 	private static final long serialVersionUID = 158223506149006607L;
 
@@ -20,7 +18,32 @@ public class Book extends Document implements Serializable {
 
 	@Override
 	public String toString() {
-		return "{_id=" + getId() + ", _rev=" + getRevision() + "}; @Book [name=" + name;
+		return "{_id=" + getId() + ", _rev=" + getRevision() + "}; @Book [name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
